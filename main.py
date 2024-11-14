0514ae18def28d1662a685d66675e48944f85b26
vitalii <it-finally@pm.me>
2024-11-14 18:43:57 +0100

    added final report

--- main.py
+++ main.py
@@ -19,17 +19,22 @@ def count_chars(text_file):
 
 def main():
 
-    path_to_books = "books/"
-    with open(path_to_books + "frankenstein.txt") as f:
+    path_to_book = "books/frankenstein.txt"
+    with open(path_to_book) as f:
         file_contents = f.read()
 
     book_words_count = count_words(file_contents)
+    print(f"--- Begins report of {path_to_book} ---")
     print(f"{book_words_count} words found in this book")
 
     book_chars_count = count_chars(file_contents)
+    sorted_dict = dict(sorted(book_chars_count.items(), key=lambda item: item[1], reverse=True))
+
     print("dictionary with counting chars:")
-    for chars in book_chars_count:
-        print(f"{chars}: {book_chars_count[chars]}")
+    for chars in sorted_dict:
+        if chars.isalpha():
+            print(f"The '{chars}' charachter was found {book_chars_count[chars]} times")
+    print("--- End report ---")
 
 if __name__ == '__main__':
     main()
